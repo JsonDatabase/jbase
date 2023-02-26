511d777ebfaa82e08616103ee04d4cbb6cf53255
Brian S Memmen <brian.memmen@gmail.com>
2023-02-26 15:10:44 -0500

    Update entity_repository_test.dart

--- test/entity_repository_test.dart
+++ test/entity_repository_test.dart
@@ -7,11 +7,11 @@ import 'package:jbase_package/src/entity_repository/entity_repository.dart';
 void main() {
   test('Create Entity', () {
     EntityRepository entityRepository = EntityRepository();
-    String json = '{"name": "John", "age": 30, "over_18": true}';
+    String json = '{"name": "John", "age": 30, "over_18": true, "height": 1.8}';
     entityRepository.addEntity('Person', json);
     expect(entityRepository.entities.length, 1);
     expect(entityRepository.entities[0].name, 'Person');
-    expect(entityRepository.entities[0].properties.length, 3);
+    expect(entityRepository.entities[0].properties.length, 4);
     expect(entityRepository.entities[0].properties[0].key, "name");
     expect(entityRepository.entities[0].properties[0].type,
         EntityPropertyType.string);
@@ -21,5 +21,8 @@ void main() {
     expect(entityRepository.entities[0].properties[2].key, "over_18");
     expect(entityRepository.entities[0].properties[2].type,
         EntityPropertyType.bool);
+    expect(entityRepository.entities[0].properties[3].key, "height");
+    expect(entityRepository.entities[0].properties[3].type,
+        EntityPropertyType.double);
   });
 }
