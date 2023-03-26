b38ba222a5c76f290cc91e8624cc39aa56f9b589
Brian S Memmen <brian.memmen@gmail.com>
2023-03-26 13:35:07 -0400

    Update jbase_package.dart

--- lib/jbase_package.dart
+++ lib/jbase_package.dart
@@ -3,3 +3,4 @@ library jbase_package;
 export 'src/control_plane/control_plane.dart';
 export 'src/control_plane/control_plane_setting.dart';
 export 'src/entity_repository/entity.dart';
+export 'src/entity_repository/entity_property.dart';
